38cdde6fad27ac66fcf582785e318551cf2bba00
Eduardo Veras <eduardo.storm@gmail.com>
2016-05-25 18:50:49 -0400

    Making the initial empty project

---
+++ src/main/java/MainPack/Main.java
@@ -0,0 +1,13 @@
+package MainPack;
+
+import spark.Spark.*;
+
+/**
+ * Created by Eduardo veras on 25-May-16.
+ *
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
