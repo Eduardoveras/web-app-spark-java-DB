a7d09e343347b0eccb6bd170f4aa08a021b205bc
Eduardo Veras <eduardo.storm@gmail.com>
2016-05-26 20:46:36 -0400

    Changes for now aparent reason

--- src/main/java/MainPack/Main.java
+++ src/main/java/MainPack/Main.java
@@ -2,12 +2,25 @@ package MainPack;
 
 import spark.Spark.*;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import static spark.Spark.get;
+
 /**
  * Created by Eduardo veras on 25-May-16.
  *
  */
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+    private static final String DB_DRIVER = "org.h2.Driver";
+    private static final String DB_CONNECTION = "jdbc:h2:~/test";
+    private static final String DB_USER = "sa";
+    private static final String DB_PASSWORD = "";
+
+
+    public static void main(String[] args) throws Exception{
+        get("/hello", (req, res) -> "<h1>Hello World</h1>");
+
     }
 }
